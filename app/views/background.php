1558cc1926992816cfc35d7cf02c850c2066c3fd
Pine Mizune <pinemz@gmail.com>
2015-12-24 00:17:00 +0900

    Update background.php

--- app/views/background.php
+++ app/views/background.php
@@ -3,13 +3,13 @@
 namespace Views;
 
 class BackgroundExtension extends \Twig_Extension {
-	private $backgrounds = [
+    private $backgrounds = [
         'yun_1375.jpg',
         'yun_7604.jpg',
         'yun_3218.jpg',
         'yun_1077.jpg',
         'yun_3281.jpg',
-	];
+    ];
 
     public function __construct()
     {
