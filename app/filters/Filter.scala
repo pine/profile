e7dc4d787571ad0bc91275d840edc6097aab0d06
Pine Mizune <pinemz@gmail.com>
2017-07-03 22:23:16 +0900

    feat(app): PHP (Slim) -> Scala (Play)

---
+++ app/filters/Filter.scala
@@ -0,0 +1,17 @@
+import javax.inject.Inject
+
+import com.mohiva.play.htmlcompressor.HTMLCompressorFilter
+import com.mohiva.play.xmlcompressor.XMLCompressorFilter
+import play.api.http.HttpFilters
+import play.api.mvc.EssentialFilter
+
+class Filters @Inject() (
+  htmlCompressorFilter: HTMLCompressorFilter,
+  xmlCompressorFilter: XMLCompressorFilter)
+  extends HttpFilters {
+
+  override def filters: Seq[EssentialFilter] = Seq(
+    htmlCompressorFilter,
+    xmlCompressorFilter
+  )
+}
