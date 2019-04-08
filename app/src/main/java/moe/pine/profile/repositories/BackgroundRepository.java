f12c7f48e58c961f1cbef860ed5cb746d4c2e3bb
Pine Mizune <pinemz@gmail.com>
2019-04-08 12:05:49 +0900

    refactor: load background images dynamically

--- app/src/main/java/moe/pine/profile/repositories/BackgroundRepository.java
+++ app/src/main/java/moe/pine/profile/repositories/BackgroundRepository.java
@@ -1,28 +1,49 @@
 package moe.pine.profile.repositories;
 
-import com.google.common.annotations.VisibleForTesting;
-import lombok.RequiredArgsConstructor;
+import com.google.common.collect.Lists;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.support.PathMatchingResourcePatternResolver;
 import org.springframework.stereotype.Repository;
 
+import javax.annotation.Nonnull;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Random;
+import java.util.stream.Collectors;
 
 @Repository
-@RequiredArgsConstructor
+@Slf4j
 public class BackgroundRepository {
+    @Nonnull
     private final Random random;
 
-    @VisibleForTesting
-    final static String[] BACKGROUNDS =
-        {
-            "yun_1077.jpg",
-            "yun_1375.jpg",
-            "yun_3218.jpg",
-            "yun_3281.jpg",
-            "yun_7604.jpg"
-        };
+    @Nonnull
+    private final ArrayList<String> backgrounds = Lists.newArrayList();
+
+    public BackgroundRepository(final Random random) {
+        this.random = random;
+
+        try {
+            final var resolver = new PathMatchingResourcePatternResolver();
+            final var resources =
+                resolver.getResources("classpath:/static/images/bg/*.jpg");
+            final var filenames =
+                Arrays.stream(resources)
+                    .map(Resource::getFilename)
+                    .collect(Collectors.toList());
+
+            log.debug("Found {} background images :: {}", filenames.size(), filenames);
+
+            this.backgrounds.addAll(filenames);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
 
     public String choose() {
-        final int index = random.nextInt(BACKGROUNDS.length);
-        return BACKGROUNDS[index];
+        final int index = random.nextInt(backgrounds.size());
+        return backgrounds.get(index);
     }
 }
