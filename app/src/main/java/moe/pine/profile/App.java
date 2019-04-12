275f29b57e740f5f3d57bacdb7bf14d0caaaeb3f
Pine Mizune <pinemz@gmail.com>
2019-04-13 00:23:41 +0900

    fix: remove unnecessary code

--- app/src/main/java/moe/pine/profile/App.java
+++ app/src/main/java/moe/pine/profile/App.java
@@ -1,17 +1,11 @@
 package moe.pine.profile;
 
-import org.apache.commons.lang3.StringUtils;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class App {
     public static void main(String[] args) {
-        final String port = System.getenv("PORT");
-        if (StringUtils.isNotEmpty(port)) {
-            System.setProperty("server.port", port);
-        }
-
         SpringApplication.run(App.class, args);
     }
 }
