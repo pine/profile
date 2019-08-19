49e84888c64ce5374e0b00f4f672345e6706c72d
Pine Mizune <pinemz@gmail.com>
2019-08-20 01:54:15 +0900

    fix: add validation

--- app/src/main/java/moe/pine/profile/properties/AnimeProperties.java
+++ app/src/main/java/moe/pine/profile/properties/AnimeProperties.java
@@ -1,13 +1,28 @@
 package moe.pine.profile.properties;
 
 import lombok.Data;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.validation.annotation.Validated;
 
+import javax.annotation.PostConstruct;
+import javax.validation.constraints.NotNull;
 import java.util.List;
 import java.util.Map;
+import java.util.Objects;
 
 @Data
+@Validated
 @ConfigurationProperties("anime")
+@Slf4j
 public class AnimeProperties {
-    private Map<String, List<String>> watched;
+    private @NotNull Map<String, List<String>> watched;
+
+    @PostConstruct
+    public void postConstruct() {
+        watched.keySet().forEach(Objects::requireNonNull);
+        watched.values().stream()
+            .flatMap(List::stream)
+            .forEach(Objects::requireNonNull);
+    }
 }
