1def5f375048732980d7d0ffab0d66b1500ceeec
Pine Mizune <pinemz@gmail.com>
2020-06-30 16:57:45 +0900

    chore: use constructor binding

--- app/src/main/java/moe/pine/profile/properties/AnimeProperties.java
+++ app/src/main/java/moe/pine/profile/properties/AnimeProperties.java
@@ -1,8 +1,9 @@
 package moe.pine.profile.properties;
 
-import lombok.Data;
-import lombok.extern.slf4j.Slf4j;
+import lombok.Value;
+import lombok.experimental.NonFinal;
 import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.context.properties.ConstructorBinding;
 import org.springframework.validation.annotation.Validated;
 
 import javax.annotation.PostConstruct;
@@ -11,12 +12,13 @@ import java.util.List;
 import java.util.Map;
 import java.util.Objects;
 
-@Data
+@Value
+@NonFinal
+@ConstructorBinding
 @Validated
 @ConfigurationProperties("anime")
-@Slf4j
 public class AnimeProperties {
-    private @NotNull Map<String, List<String>> watched;
+    @NotNull Map<String, List<String>> watched;
 
     @PostConstruct
     public void postConstruct() {
